333925f1b57885add55bd9223f833b5f81bf55e7
Serhii/Gray <vasiliskgari@gmail.com>
2025-09-17 15:35:26 +0200

    Js_3.2

---
+++ 3.0/3.2.js
@@ -0,0 +1,18 @@
+var name = 'Ivan';
+console.log (name);
+
+var number = 10;
+console.log(number);
+
+let age = 10;
+console.log(age);
+
+let message = 'HalloWorld';
+console.log(message);
+
+let person ={
+    name: 'Jack',
+    age: 25,
+    message: 'Hallo';
+}
+console.log(person);
