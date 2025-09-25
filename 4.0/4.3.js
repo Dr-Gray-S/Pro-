9bbb776106b11211009c2f3396f08a3f24131607
Serhii/Gray <vasiliskgari@gmail.com>
2025-09-25 16:09:31 +0200

    Add 4.0 tasks

---
+++ 4.0/4.3.js
@@ -0,0 +1,16 @@
+const year = prompt("Сколько вам лет ? ");
+const city = prompt("Откуда вы ? ");
+const sport = prompt("Какой ваш любимый вид спорта  ? ");
+
+let cityMsg = 'Ты живешь в ${city}';
+const cl = city.trim().toLowerCase();
+
+if (city === 'Киев') {
+    cityMsg ('Ты живешь в столице Украины')
+} else (сity === 'Вашингтон'){
+    cityMsg ('Ты живешь в столице США')
+} else (сity === 'Лондон'){
+    cityMsg ('Ты живешь в столице Англии')
+}
+
+alert (`Твой возраст: $[year].\n${cityMsg}\nТвой любимый вид спрта: ${sport}.`);
